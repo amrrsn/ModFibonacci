f25b2a0e2fab46753303d3d48fc192e6d827fbad
Alan Morrison <bumblebeealan01@gmail.com>
2022-03-11 10:12:23 -0500

    added saving of the numpy arrays to skip having to re-make them every time

---
+++ main.py
@@ -0,0 +1,82 @@
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+import os.path
+
+range_start = 2
+range_end = 1000
+loop_count = range_end - range_start
+contains_x = np.arange(stop=loop_count, dtype=int)
+contains_y = np.zeros(loop_count, dtype=int)
+doesnt_contain_x = np.arange(stop=loop_count, dtype=int)
+doesnt_contain_y = np.zeros(loop_count, dtype=int)
+
+data_dir = f'data/{loop_count}/'
+
+
+def check_sequence(modulus: int) -> (bool, int, int):
+    remainders = np.arange(stop=modulus, dtype=int)
+
+    count = 2
+    last_digits = [0, 1]
+    while True:
+        count += 1
+        last_digits = [last_digits[1], (last_digits[0] + last_digits[1]) % modulus]
+        if last_digits[1] in remainders:
+            remainders = np.delete(remainders, np.where(remainders == last_digits[1]))
+        ###
+        # optimizes the loop by skipping the rest of the loop if the sequence is found
+        # this is because the sequence is found when the remainders array is empty
+        # commented out because this case is rare in the long run
+        # and it is preferable to keep the count consistent for the graph
+        ###
+        # if not remainders.size:
+        #     contains_y[modulus-range_start] = count
+        #     return True
+        if last_digits == [0, 1]:
+            break
+
+    if not remainders.size:
+        return True, modulus, count
+    return False, modulus, count
+
+
+if __name__ == '__main__' and not os.path.exists(f"{data_dir}/contains_x_{loop_count}.npy"):
+    contains_all = 0
+    doesnt_contain_all = 0
+    pool = mp.Pool(processes=mp.cpu_count())
+    for i, mod, ct in tqdm(pool.imap_unordered(check_sequence, np.arange(range_start, range_end, dtype=int)), total=loop_count):
+        contains_y[mod - range_start] = i*ct
+        doesnt_contain_y[mod - range_start] = (not i)*ct
+        contains_all += i
+        doesnt_contain_all += (not i)
+    pool.close()
+
+    contains_x = np.delete(contains_x, np.where(contains_y == 0))
+    contains_y = np.delete(contains_y, np.where(contains_y == 0))
+    doesnt_contain_x = np.delete(doesnt_contain_x, np.where(doesnt_contain_y == 0))
+    doesnt_contain_y = np.delete(doesnt_contain_y, np.where(doesnt_contain_y == 0))
+
+    if not os.path.exists(f'{data_dir}/contains_x_{loop_count}.npy'):
+        np.save(f'{data_dir}/contains_y_{loop_count}.npy', contains_y)
+        np.save(f'{data_dir}/doesnt_contain_x_{loop_count}.npy', doesnt_contain_x)
+        np.save(f'{data_dir}/doesnt_contain_y_{loop_count}.npy', doesnt_contain_y)
+        np.save(f'{data_dir}/contains_x_{loop_count}.npy', contains_x)
+
+    print(f"{(contains_all+1)}/{contains_all+doesnt_contain_all+1} = {(contains_all+1)/(contains_all + doesnt_contain_all+1)*100}% of the moduli contain every remainder.")
+
+if __name__ == '__main__':
+    if os.path.exists(f'{data_dir}/contains_x_{loop_count}.npy'):
+        contains_x = np.load(f'{data_dir}/contains_x_{loop_count}.npy')
+        contains_y = np.load(f'{data_dir}/contains_y_{loop_count}.npy')
+        doesnt_contain_x = np.load(f'{data_dir}/doesnt_contain_x_{loop_count}.npy')
+        doesnt_contain_y = np.load(f'{data_dir}/doesnt_contain_y_{loop_count}.npy')
+
+    plt.plot(doesnt_contain_x, doesnt_contain_y, 'yo', label="Doesn't contain every remainder")
+    plt.plot(contains_x, contains_y, 'kx', label="Contains every remainder")
+    plt.xlabel("Modulus")
+    plt.ylabel("Iterations")
+    plt.legend(fontsize=10)
+    plt.title("Fibonacci modulus iterations before repeating sequence")
+    plt.show()
